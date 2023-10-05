5127fdf8fa27d92d92da9df368b39e0619c81b36
Rodolfo F <rodolfo_frainer@hotmail.com>
2023-10-05 17:37:48 +0200

    add comments

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+from classes import PlayerClass, PipeClass
 import random
 import os
 
 import pygame
+pygame.font.init()
 
-from classes import PlayerClass, PipeClass
 
 # CONSTANTS
 SCREEN_SIZE = (750, 1000)
 FPS = 144
 GRAVITY = 3
 PLAYER_MAX_SPEED = 20
-
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_SIZE))
@@ -18,13 +19,15 @@ clock = pygame.time.Clock()
 running = True
 
 
-#PLAYER
+# PLAYER
 player_image = pygame.transform.scale2x(
     pygame.image.load(os.path.join("images", "plaffyDirb.png")))
 player = PlayerClass(SCREEN_SIZE[0] / 8, SCREEN_SIZE[1] / 2, 30, player_image)
 
-#PIPES
+# PIPES
 pipes = []
+
+
 def spawn_pipe(frame_interation, frequency):
     if frame_interation % (FPS * frequency) == 0:
         pipe_x = SCREEN_SIZE[0]
@@ -47,13 +50,16 @@ def spawn_pipe(frame_interation, frequency):
                           pygame.transform.flip(pipe_image, False, True))
         pipes.append(pipe2)
 
-#Draw screen
-def frame_draw():
+# Draw screen
+
+
+def frame_draw(score):
     PLAYER_POSITION = (player.x-player.image.get_width()/2,
                        player.y-player.image.get_height()/2)
-    
+
     screen.fill("blue")
     screen.blit(player.image, PLAYER_POSITION)
+
     for pipe in pipes:
         screen.blit(pipe.image, (pipe.x, pipe.y))
     if len(pipes) > 0:
@@ -84,8 +90,14 @@ def frame_draw():
                              (pipes[-1].x+pipes[-1].image.get_width()/2,
                               pipes[-1].y+pipes[-1].image.get_height()))
 
+    # render score
+    score_label = STAT_FONT.render(
+        "Score: " + str(int(score)), 1, (255, 255, 255))
+    screen.blit(
+        score_label, (SCREEN_SIZE[0] - score_label.get_width() - 15, 10))
 
-#update instances
+
+# update instances
 def instances_update():
     player.y += GRAVITY
     for pipe in pipes:
@@ -98,23 +110,25 @@ def can_player_jump():
             player.jump()
 
 
+# detect if player and pipes colided
 def collision_detection(player, pipes):
     player_mask = pygame.mask.from_surface(player.image)
     for pipe in pipes:
         pipe_mask = pygame.mask.from_surface(pipe.image)
         if pipe_mask.overlap(player_mask, (player.x-player.radius-pipe.x, player.y-player.radius-pipe.y)):
             game_over()
-            pass  # need to handle collision for
+            pass  # need to handle collision for AI
 
-
+# Breaks game loop and finish program
 def game_over(running=False):
     running = False
     pygame.quit()
     quit()
 
 
-# GAME------------------------
+################### GAME ####################
 frame_interation = FPS
+score = 0
 while running:
     frame_interation += 1
     keys = pygame.key.get_pressed()
@@ -124,10 +138,12 @@ while running:
     if player.y+player.image.get_height()/20 >= SCREEN_SIZE[1]:
         game_over()
 
-    
     can_player_jump()
 
     for pipe in pipes:
+        if round(player.x) == round(pipe.x):
+            score += 0.5
+
         if pipe.x <= 0 - pipe.image.get_width():
             pipes.remove(pipe)
 
@@ -136,7 +152,7 @@ while running:
     # Update instances positions
     instances_update()
     # draw the frame
-    frame_draw()
+    frame_draw(score)
 
     # detects if player mask and pipe mask collide
     collision_detection(player, pipes)
